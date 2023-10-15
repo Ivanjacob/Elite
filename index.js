442ed925a7e9c5ef09875579795f23041c59f242
Evan <77968134+Ivanjacob@users.noreply.github.com>
2023-10-15 16:19:40 +0300

    About done

---
+++ index.js
@@ -0,0 +1,15 @@
+// Type: JavaScript
+  var typed = new Typed('#typed', {
+    strings: ['Python Developer', 'Web Developer', 'Data Anlyst', 'Article Writer', 'Academic Writer', 'Freelancer'],
+    stringsElement: null,
+    typeSpeed: 10,
+    startDelay: 150,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true,
+    loopCount: 550,
+    showCursor: true,
+    cursorChar: "|",
+    attr: null,
+    contentTypeL: 'html',
+});
